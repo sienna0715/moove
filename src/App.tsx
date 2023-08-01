1b73e3a5b28af2984ef6fd6b4eee7c33f4bceeb5
sienna0715 <5609373@naver.com>
2023-08-01 23:26:58 +0900

    ✨feat/Apply react-zoom-pan-pinch library

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import './App.css';
 import Line2 from './components/Line2';
 import Line4 from './components/Line4';
 import Line5 from './components/Line5';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 function App() {
   return (
-    <svg width={2700} height={1800}>
-      <Line2 />
-      <Line4 />
-      <Line5 />
-      <use xlinkHref="#line2SvgContainer" />
-      <use xlinkHref="#line4SvgContainer" />
-      <use xlinkHref="#line5SvgContainer" y={100} />
-    </svg>
+    <TransformWrapper initialScale={1} minScale={1} maxScale={10}>
+      <TransformComponent>
+        <svg width={2700} height={1800}>
+          <Line2 />
+          <Line4 />
+          <Line5 />
+          <use xlinkHref="#line2SvgContainer" />
+          <use xlinkHref="#line4SvgContainer" />
+          <use xlinkHref="#line5SvgContainer" y={100} />
+        </svg>
+      </TransformComponent>
+    </TransformWrapper>
   );
 }
 
