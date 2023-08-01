b7cb79ac6c3d7247ae83ff0dacf220be52958bef
lapmu <antjdgkdl@gmail.com>
2023-08-01 16:24:17 +0900

    ✨feat/line2, line4 적용후 위치 조정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css';
+import Line2 from './components/Line2';
+import Line4 from './components/Line4';
 import Line5 from './components/Line5';
 
 function App() {
   return (
     <svg width={3000} height={1200}>
+      <Line2 />
+      <Line4 />
+      <use xlinkHref="#line2SvgContainer" x={388} y={-50} />
+      <use xlinkHref="#line4SvgContainer" x={388} y={-50} />
       <Line5 />
     </svg>
   );
---
+++ src/components/Line4.tsx
@@ -0,0 +1,98 @@
+function Line4() {
+  return (
+    <symbol id="line4SvgContainer">
+      <line
+        x1={700}
+        y1={50}
+        x2={1150}
+        y2={50}
+        strokeWidth="8"
+        stroke="#009FE0"
+      />
+      <path
+        d="M700,50 C700,50 650,50 650,100"
+        fill="none"
+        stroke="#009FE0"
+        strokeWidth="8"
+      />
+      <line
+        x1={650}
+        y1={100}
+        x2={650}
+        y2={250}
+        strokeWidth="8"
+        stroke="#009FE0"
+      />
+      <path
+        d="M650,250 C650,250 650,300 600,300"
+        fill="none"
+        stroke="#009FE0"
+        strokeWidth="8"
+      />
+      <line
+        x1={600}
+        y1={300}
+        x2={550}
+        y2={300}
+        strokeWidth="8"
+        stroke="#009FE0"
+      />
+      <path
+        d="M550,300 C475,300 475,400 550,400"
+        fill="none"
+        stroke="#009FE0"
+        strokeWidth="8"
+      />
+      <path
+        d="M550,400 C550,400 600,400 600,450"
+        fill="none"
+        stroke="#009FE0"
+        strokeWidth="8"
+      />
+      <line
+        x1={600}
+        y1={450}
+        x2={600}
+        y2={800}
+        strokeWidth="8"
+        stroke="#009FE0"
+      />
+      <path
+        d="M600,800 C600,850 550,850 550,850"
+        fill="none"
+        stroke="#009FE0"
+        strokeWidth="8"
+      />
+      <line
+        x1={550}
+        y1={850}
+        x2={200}
+        y2={850}
+        strokeWidth="8"
+        stroke="#009FE0"
+      />
+      <path
+        d="M200,850 C200,850 150,850 150,900"
+        fill="none"
+        stroke="#009FE0"
+        strokeWidth="8"
+      />
+      <line
+        x1={150}
+        y1={900}
+        x2={150}
+        y2={1050}
+        strokeWidth="8"
+        stroke="#009FE0"
+      />
+      <path
+        d="M150,1050 C150,1150 0,1150 0,1050"
+        fill="none"
+        stroke="#009FE0"
+        strokeWidth="8"
+      />
+    </symbol>
+  );
+}
+
+export default Line4;
