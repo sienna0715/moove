dc0ed6fc533f9c3bf7e22d8972769e9d55fb7a07
lapmu <antjdgkdl@gmail.com>
2023-08-01 12:20:32 +0900

    🐛fix/svg 태그 Line5에서 app으로 위치 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import './App.css';
 import Line5 from './components/Line5';
 
 function App() {
-  return <Line5 />;
+  return (
+    <svg width={3000} height={1200}>
+      <Line5 />
+    </svg>
+  );
 }
 
 export default App;
