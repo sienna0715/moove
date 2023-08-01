086c8782ac610352feae6b40b193b5e80827c547
sienna0715 <5609373@naver.com>
2023-08-01 11:37:42 +0900

    ✨feat/폴더 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css';
-import Line5 from './Line5';
+import Line5 from './components/Line5';
 
 function App() {
-  return <Line5 />;
+  return (
+    <>
+      <Line5 />
+    </>
+  );
 }
 
 export default App;
