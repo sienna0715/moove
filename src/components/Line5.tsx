a6c34facdf6aaafeabadb79ff9de5d11a92a9502
sienna0715 <5609373@naver.com>
2023-08-01 15:53:46 +0900

    ✨feat/Line5 정류장 svg 구현

--- src/components/Line5.tsx
+++ src/components/Line5.tsx
@@ -9,61 +9,65 @@ function Line5() {
         strokeWidth={8}
         strokeLinecap="round"
       />
+      {/* 마곡 */}
       <line
         x1={170}
         y1={80}
         x2={170}
-        y2={350}
+        y2={550}
         strokeWidth={8}
         stroke="#8300EB"
       />
       <path
-        d="M170 350 C 170 350, 170 420, 250 420"
+        d="M170 550 C 170 550, 170 600, 250 600"
         fill="none"
         stroke="#8300EB"
         strokeWidth={8}
         strokeLinecap="round"
       />
+      {/* 영등포구청 */}
       <line
         x1={250}
-        y1={420}
-        x2={550}
-        y2={420}
+        y1={600}
+        x2={600}
+        y2={600}
         strokeWidth={8}
         stroke="#8300EB"
       />
+      {/* 신길 */}
       <path
-        d="M550 420 C 550 420, 600 420, 640 470 S 750 500, 750 420"
+        d="M600 600 C 600 600, 635 600, 675 650 S 780 680, 780 600"
         fill="none"
         stroke="#8300EB"
         strokeWidth={8}
         strokeLinecap="round"
       />
+      {/* 여의도 */}
       <line
-        x1={750}
+        x1={780}
         y1={80}
-        x2={750}
-        y2={420}
+        x2={780}
+        y2={600}
         strokeWidth={8}
         stroke="#8300EB"
       />
       <path
-        d="M750 80 C 750 80, 750 10, 800 10"
+        d="M780 80 C 780 80, 780 30, 830 30"
         fill="none"
         stroke="#8300EB"
         strokeWidth={8}
         strokeLinecap="round"
       />
       <line
-        x1={800}
-        y1={10}
+        x1={830}
+        y1={30}
         x2={850}
-        y2={10}
+        y2={30}
         strokeWidth={8}
         stroke="#8300EB"
       />
       <path
-        d="M850 10 C 850 10, 900 10, 900 80"
+        d="M850 30 C 850 30, 900 30, 900 80"
         fill="none"
         stroke="#8300EB"
         strokeWidth={8}
@@ -152,14 +156,6 @@ function Line5() {
         strokeWidth={8}
         stroke="#8300EB"
       />
-      <line
-        x1={2000}
-        y1={500}
-        x2={2000}
-        y2={600}
-        strokeWidth={8}
-        stroke="#8300EB"
-      />
       <path
         d="M1900 700 C 1900 700, 2000 700, 2000 600"
         fill="none"
@@ -167,8 +163,16 @@ function Line5() {
         strokeWidth={8}
         strokeLinecap="round"
       />
+      <line
+        x1={2000}
+        y1={610}
+        x2={2000}
+        y2={600}
+        strokeWidth={8}
+        stroke="#8300EB"
+      />
       <path
-        d="M2000 500 C 2000 500, 2000 400, 2100 400"
+        d="M2000 600 C 2000 600, 2000 500, 2100 500"
         fill="none"
         stroke="#8300EB"
         strokeWidth={8}
@@ -177,9 +181,24 @@ function Line5() {
       {/* 하남풍산 */}
       <line
         x1={2100}
-        y1={400}
-        x2={2200}
-        y2={400}
+        y1={500}
+        x2={2300}
+        y2={500}
+        strokeWidth={8}
+        stroke="#8300EB"
+      />
+      <path
+        d="M2300 500 C 2300 500, 2400 500, 2400 600"
+        fill="none"
+        stroke="#8300EB"
+        strokeWidth={8}
+        strokeLinecap="round"
+      />
+      <line
+        x1={2400}
+        y1={600}
+        x2={2400}
+        y2={650}
         strokeWidth={8}
         stroke="#8300EB"
       />
@@ -195,12 +214,12 @@ function Line5() {
         x1={2100}
         y1={800}
         x2={2100}
-        y2={900}
+        y2={930}
         strokeWidth={8}
         stroke="#8300EB"
       />
       <path
-        d="M2100 900 C 2100 900, 2100 1000, 2200 1000"
+        d="M2100 930 C 2100 930, 2100 1000, 2200 1000"
         fill="none"
         stroke="#8300EB"
         strokeWidth={8}
@@ -209,12 +228,12 @@ function Line5() {
       <line
         x1={2200}
         y1={1000}
-        x2={2350}
+        x2={2400}
         y2={1000}
         strokeWidth={8}
         stroke="#8300EB"
       />
-      {/* station */}
+      {/* --------- station --------- */}
       <circle
         cx={10}
         cy={10}
@@ -224,7 +243,43 @@ function Line5() {
         stroke="#8300EB"
       />
       <circle
-        cx={2200}
+        cx={151}
+        cy={25}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 김포공항 */}
+      <rect
+        width={15}
+        height={40}
+        x={162}
+        y={100}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={170}
+        cy={220}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={170}
+        cy={310}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={170}
         cy={400}
         r={5}
         fill="white"
@@ -232,7 +287,466 @@ function Line5() {
         stroke="#8300EB"
       />
       <circle
-        cx={2350}
+        cx={170}
+        cy={490}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={185}
+        cy={580}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 까치산 */}
+      <rect
+        width={15}
+        height={30}
+        x={275}
+        y={585}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={335}
+        cy={600}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={395}
+        cy={600}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={455}
+        cy={600}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={515}
+        cy={600}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 영등포구청 */}
+      <rect
+        width={15}
+        height={30}
+        x={575}
+        y={585}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={660}
+        cy={634}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 신길 */}
+      <rect
+        width={15}
+        height={30}
+        x={720}
+        y={665}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 여의도 */}
+      <rect
+        width={15}
+        height={30}
+        x={772}
+        y={560}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={780}
+        cy={520}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={780}
+        cy={450}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 공덕 */}
+      <rect
+        width={15}
+        height={40}
+        x={772}
+        y={370}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={780}
+        cy={300}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 충정로 */}
+      <rect
+        width={15}
+        height={30}
+        x={772}
+        y={190}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={780}
+        cy={100}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={840}
+        cy={30}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 종로3가 */}
+      <rect
+        width={40}
+        height={15}
+        x={890}
+        y={90}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 을지로4가 */}
+      <rect
+        width={15}
+        height={30}
+        x={950}
+        y={190}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 동대문역사문화공원 */}
+      <rect
+        width={15}
+        height={40}
+        x={1030}
+        y={190}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 청구 */}
+      <rect
+        width={15}
+        height={30}
+        x={1190}
+        y={260}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={1250}
+        cy={270}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={1320}
+        cy={240}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 왕십리 */}
+      <rect
+        width={40}
+        height={15}
+        x={1400}
+        y={190}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={1510}
+        cy={200}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={1590}
+        cy={200}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={1670}
+        cy={227}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 군자 */}
+      <rect
+        width={30}
+        height={15}
+        x={1680}
+        y={300}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={1700}
+        cy={420}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={1700}
+        cy={530}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 천호 */}
+      <rect
+        width={30}
+        height={15}
+        x={1700}
+        y={630}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 강동 */}
+      <rect
+        width={30}
+        height={15}
+        x={1890}
+        y={693}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={1990}
+        cy={650}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={2002}
+        cy={580}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={2030}
+        cy={525}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 고덕 */}
+      <circle
+        cx={2085}
+        cy={500}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={2145}
+        cy={500}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={2205}
+        cy={500}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={2265}
+        cy={500}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={2325}
+        cy={503}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={2394}
+        cy={560}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 하남검단산 */}
+      <circle
+        cx={2400}
+        cy={650}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 둔촌동 */}
+      <circle
+        cx={1990}
+        cy={700}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 올림픽공원 */}
+      <rect
+        width={30}
+        height={15}
+        x={2085}
+        y={755}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={2100}
+        cy={845}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      {/* 오금 */}
+      <rect
+        width={15}
+        height={30}
+        x={2093}
+        y={915}
+        rx={8}
+        ry={8}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={2200}
+        cy={1000}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={2300}
+        cy={1000}
+        r={5}
+        fill="white"
+        strokeWidth={5}
+        stroke="#8300EB"
+      />
+      <circle
+        cx={2400}
         cy={1000}
         r={5}
         fill="white"
