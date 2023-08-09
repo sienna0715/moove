e871c8c347876b10c96da4aba11d29c6a997ef90
sienna0715 <5609373@naver.com>
2023-08-09 16:31:53 +0900

    ✨feat/ 상세보기 하단 컴포넌트 css 작업

--- src/components/DetailStation.tsx
+++ src/components/DetailStation.tsx
@@ -1,7 +1,12 @@
 import '../styles/DetailStation.scss';
+import StationResult from './StationResult';
 
 function DetailStation() {
-  return <div className="DetailStationWrap">DetailStation</div>;
+  return (
+    <div className="DetailStationWrap">
+      <StationResult />
+    </div>
+  );
 }
 
 export default DetailStation;
