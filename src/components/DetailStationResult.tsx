6e24b2b0e50ca903e33d6a0049b0b92902734c08
sienna0715 <5609373@naver.com>
2023-08-11 17:05:44 +0900

    🐛fix/detailStationReault 컴포넌트 조건문 수정

--- src/components/DetailStationResult.tsx
+++ src/components/DetailStationResult.tsx
@@ -5,44 +5,55 @@ import axios from 'axios';
 import '../styles/DetailStationResultStyle.scss';
 import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md';
 import { currentStationAtom, stationInfoAtom } from '../recoil/atoms';
-import { ISubwayProps } from '../interfaces/interfaces';
+import { ISubwayProps, ISubwayArriveInfo } from '../interfaces/interfaces';
 
 export default function DetailStationResult() {
   const currentStation = useRecoilValue(currentStationAtom);
-  const [stationInfo, setStationInfo] = useRecoilState(stationInfoAtom);
-  let subwayId: string;
-  console.log(stationInfo[0]?.subwayId, stationInfo[0]?.subwayList.split(','));
+  const [stationInfo, setStationInfo] = useRecoilState<ISubwayArriveInfo[]>(stationInfoAtom);
+  let subwayId = 'transfer';
+  const line4 = ['별내별가람', '오남', '진접'];
+  const line5 = ['미사', '하남풍산', '하남시청', '하남검단산', '개롱', '거여'];
+  // api address
+  const arrivalAddress = 'http://swopenAPI.seoul.go.kr/api/subway/6267466b6b616e7437394670434955/json/realtimeStationArrival/0/5/';
+  // const tourAddress = 'http://openapi.seoul.go.kr:8088/4157654b4a616e743839737359467a/json/subwayTourInfo/1/5/';
 
-  if (Number(stationInfo[0]?.trnsitCo) > 1) {
-    subwayId = 'transfer';
-  } else if (Number(stationInfo[0]?.trnsitCo) === 1) {
-    if (
-      stationInfo[0]?.subwayList.split(',').includes('1002')
-      || stationInfo[0]?.subwayId === '1002'
-    ) {
-      subwayId = '1002';
-    } else if (
-      stationInfo[0]?.subwayList.split(',').includes('1004')
-      || stationInfo[0]?.subwayId === '1004'
-    ) {
-      subwayId = '1004';
-    } else if (
-      stationInfo[0]?.subwayList.split(',').includes('1005')
-      || stationInfo[0]?.subwayId === '1005'
-      || stationInfo[0]?.subwayId === '1063'
-    ) {
-      subwayId = '1005';
+  if (stationInfo) {
+    if (Number(stationInfo[0]?.trnsitCo) > 1) {
+      subwayId = 'transfer';
+    } else if (Number(stationInfo[0]?.trnsitCo) === 1) {
+      if (
+        stationInfo[0]?.subwayList.split(',').includes('1002')
+        || stationInfo[0]?.subwayId === '1002'
+      ) {
+        subwayId = '1002';
+      } else if (
+        stationInfo[0]?.subwayList.split(',').includes('1004')
+        || line4.includes(currentStation)
+        || stationInfo[0]?.subwayId === '1004'
+      ) {
+        subwayId = '1004';
+      } else if (
+        stationInfo[0]?.subwayList.split(',').includes('1005')
+        || line5.includes(currentStation)
+        || stationInfo[0]?.subwayId === '1005'
+        || stationInfo[0]?.subwayId === '1063'
+      ) {
+        subwayId = '1005';
+      }
     }
+  } else if (stationInfo === undefined) {
+    subwayId = 'transfer';
   }
-
-  // 5호선 - 신정, 오목교, 군자, 아차산, 광나루, 천호, 굽은다리, 강일, 미사, 하남풍산, 하남시청, 하남검단산, 개롱, 거여
-  // 4호선 - 별내별가람, 오남, 진접
 
   useEffect(() => {
     axios
-      .get<ISubwayProps>(`http://swopenAPI.seoul.go.kr/api/subway/6267466b6b616e7437394670434955/json/realtimeStationArrival/0/5/${currentStation}`)
+      .get<ISubwayProps>(`${arrivalAddress}${currentStation}`)
       .then((res) => setStationInfo(res.data.realtimeArrivalList))
       .catch((err) => console.log(err));
+    // axios
+    //   .get(`${stationIdAddress}`)
+    //   .then((res) => console.log(res.data.subwayTourInfo.row))
+    //   .catch((err) => console.log(err));
   }, [currentStation, setStationInfo]);
 
   return (
@@ -53,18 +64,26 @@ export default function DetailStationResult() {
           <div className={`sub-station line${subwayId}`}>
             <span className="pre-station">
               <MdArrowBackIos />
-              당산
             </span>
             <span className="next-station">
-              홍대입구
               <MdArrowForwardIos />
             </span>
           </div>
         </div>
-        <div className="arrive-information">
-          <span className="subway">성수(외선행)</span>
-          <span className="count">3분 21초</span>
-        </div>
+        {stationInfo?.map((arrive) => (
+          <div className="arrive-information">
+            <div>
+              <span className="subway">{arrive.trainLineNm.split('-')[0]}</span>
+              <span className="subway">
+                &#40;
+                {arrive.updnLine}
+                &#41;
+              </span>
+            </div>
+            <span className="count">{arrive.arvlMsg2}</span>
+          </div>
+        ))}
+
         <div className="exit-information">
           <span className="exit-title">5번 출구</span>
           <ul className="exit-list">
