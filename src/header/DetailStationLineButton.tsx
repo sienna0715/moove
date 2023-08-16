da483df8f2ebd9b00f81e9933c8b42fca5e40b31
lapmu <antjdgkdl@gmail.com>
2023-08-16 16:45:02 +0900

    ♻️refactor/button컴포넌트에 각각있던 recoilvalue와상태변환함수 수정

--- src/header/DetailStationLineButton.tsx
+++ src/header/DetailStationLineButton.tsx
@@ -1,4 +1,4 @@
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { useEffect, useState } from 'react';
 // components
 import { lineSelectAtom } from '../recoil/atoms';
@@ -9,19 +9,18 @@ function DetailStationLineButton({
   line,
 }: IDetailStationLineButtonProps) {
   const [isLine, setIsLine] = useState('true');
-  const setLineSelect = useSetRecoilState(lineSelectAtom);
-  const lineSelectValue = useRecoilValue(lineSelectAtom);
+  const [lineSelect, setLineSelect] = useRecoilState(lineSelectAtom);
 
   useEffect(() => {
-    if (lineSelectValue === line || lineSelectValue === '') {
+    if (lineSelect === line || lineSelect === '') {
       setIsLine('true');
     } else {
       setIsLine('false');
     }
-  }, [line, lineSelectValue]);
+  }, [line, lineSelect]);
 
   const lineButtonClickHandler = () => {
-    if (lineSelectValue === line) {
+    if (lineSelect === line) {
       setLineSelect('');
     } else {
       setLineSelect(line);
