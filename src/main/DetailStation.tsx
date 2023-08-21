a0d60ec64d49222828d79590e4dd2bd053dd492c
lapmu <antjdgkdl@gmail.com>
2023-08-21 11:46:44 +0900

    🐛fix/배포 사이트에서 cors에러 3차 수정

--- src/main/DetailStation.tsx
+++ src/main/DetailStation.tsx
@@ -7,7 +7,6 @@ import { currentStationAtom, stationInfoAtom } from '../recoil/atoms';
 import { ISubwayProps, ISubwayArriveInfo } from '../interfaces/interfaces';
 import DetailStationLocation from './DetailStationLocation';
 import DetailStationInfo from './DetailStationInfo';
-import DetailStationExit from './DetailStationExit';
 
 export default function DetailStation() {
   const currentStation = useRecoilValue(currentStationAtom);
@@ -16,11 +15,11 @@ export default function DetailStation() {
 
   // api address
   const arrivalAddress =
-    'http://swopenAPI.seoul.go.kr/api/subway/6267466b6b616e7437394670434955/json/realtimeStationArrival/0/5/';
+    '/api/subway/6267466b6b616e7437394670434955/json/realtimeStationArrival/0/5/';
 
   useEffect(() => {
     axios
-      .get<ISubwayProps>(`https://cors-anywhere.herokuapp.com/${arrivalAddress}${currentStation}`)
+      .get<ISubwayProps>(`${arrivalAddress}${currentStation}`)
       .then((res) => setStationInfo(res.data.realtimeArrivalList))
       .catch((err) => console.log(err));
   }, [currentStation, setStationInfo]);
@@ -30,9 +29,7 @@ export default function DetailStation() {
       <div className="detail-station-container">
         <DetailStationLocation />
         <DetailStationInfo />
-        <DetailStationExit />
       </div>
     </div>
   );
 }
-
